b7031918a527c84a17ee5bb55ec3bc6a36b5f424
mintaq <minh.tranquang98@gmail.com>
2020-01-30 16:00:01 +0700

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!');
     } else {
-      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out there. There is ${body.currently.precipProbability}% chance of rain.`);
+      callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out there. The high to day is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is ${body.currently.precipProbability}% chance of rain.`);
     }
   });
 };
